e5125482b897d4121015991af47cc12b71cb31f7
UuuNyaa <uuunyaa@gmail.com>
2022-11-30 14:01:04 +0000

    Output the results to JSON

--- motion_generate_tools/motion_edit_test.py
+++ motion_generate_tools/motion_edit_test.py
@@ -3,18 +3,20 @@
 Generate a large batch of image samples from a model and save them as a large
 numpy array. This can be used to produce samples for FID evaluation.
 """
+import json
 import logging
 import os
 
+import numpy as np
 import torch
-from torch.utils import data
+from data_loaders.humanml.utils import paramUtil
 
 from mdm.data_loaders.humanml.data.dataset import HumanML3D
 from mdm.data_loaders.humanml.scripts import motion_process
 from mdm.data_loaders.tensors import t2m_collate
 from mdm.model import cfg_sampler
 from mdm.utils import dist_util, fixseed, model_util
-from motion_utils import DATA_PATH, dotdict
+from motion_utils import DATA_PATH, NumpyJSONEncoder, dotdict
 
 SMPL_DATA_PATH = os.path.join(DATA_PATH, 'body_models', 'smpl')
 SMPL_MODEL_PATH = os.path.join(SMPL_DATA_PATH, 'SMPL_NEUTRAL.pkl')
@@ -31,7 +33,6 @@ def main():
     device = 0
     text_condition = "a person jumps"
     num_samples = 1
-    num_repetitions = 1
     batch_size = 1
     dataset = 'humanml'
     prefix_end = 0.25
@@ -41,11 +42,7 @@ def main():
 
     dist_util.setup_dist(device)
 
-    data_loader = data.DataLoader(
-        HumanML3D(mode='train', base_path=DATA_PATH, split='test', num_frames=max_frames),
-        batch_size=batch_size, shuffle=True,
-        num_workers=8, drop_last=True, collate_fn=t2m_collate
-    )
+    humanml3d = HumanML3D(mode='train', base_path=DATA_PATH, split='test', num_frames=max_frames)
 
     logging.info("Creating model and diffusion...")
     model = model_util.MDM(
@@ -58,7 +55,7 @@ def main():
             'cond_mask_prob': 0.1,
             'arch': 'trans_enc',
             'emb_trans_dec': False,
-        }), data_loader)
+        }), dotdict({'num_actions': 1, }))
     )
 
     diffusion = model_util.create_gaussian_diffusion(dotdict({
@@ -77,63 +74,100 @@ def main():
     model.to(dist_util.dev())
     model.eval()  # disable random masking
 
-    input_motions, model_kwargs = next(iter(data_loader))
+    # input_motions, model_kwargs = next(iter(data_loader))
+    input_motions, model_kwargs = t2m_collate([humanml3d[0]])
     input_motions = input_motions.to(dist_util.dev())
 
-    model_kwargs['y']['text'] = [text_condition] * num_samples
     if text_condition == '':
         guidance_param = 0.  # Force unconditioned generation
 
+    lenbatchTensor = model_kwargs['y']['lengths']
+
     # add inpainting mask according to args
     assert max_frames == input_motions.shape[-1]
-    model_kwargs['y']['inpainted_motion'] = input_motions
-    model_kwargs['y']['inpainting_mask'] = torch.ones_like(
+    inpainting_mask = torch.ones_like(
         input_motions,
         dtype=torch.bool,
         device=input_motions.device
     )  # True means use gt motion
-
-    for i, length in enumerate(model_kwargs['y']['lengths'].cpu().numpy()):
+    for i, length in enumerate(lenbatchTensor.cpu().numpy()):
         start_idx, end_idx = int(prefix_end * length), int(suffix_start * length)
-        model_kwargs['y']['inpainting_mask'][i, :, :, start_idx: end_idx] = False  # do inpainting in those frames
+        inpainting_mask[i, :, :, start_idx: end_idx] = False  # do inpainting in those frames
 
-    all_motions = []
-    all_lengths = []
-    all_text = []
+    logging.info('### Start sampling')
 
-    for rep_i in range(num_repetitions):
-        logging.info(f'### Start sampling [repetitions #{rep_i}]')
+    sample_fn = diffusion.p_sample_loop
 
-        # add CFG scale to batch
-        model_kwargs['y']['scale'] = torch.ones(batch_size, device=dist_util.dev()) * guidance_param
+    sample = sample_fn(
+        model,
+        (batch_size, model.njoints, model.nfeats, max_frames),
+        clip_denoised=False,
+        model_kwargs={'y': {
+            'text': [text_condition] * num_samples,
+            'inpainted_motion': input_motions,
+            'inpainting_mask': inpainting_mask,
+            'scale': torch.ones(batch_size, device=dist_util.dev()) * guidance_param,  # add CFG scale to batch
+        }},
+        skip_timesteps=0,  # 0 is the default value - i.e. don't skip any step
+        init_image=None,
+        progress=True,
+        dump_steps=None,
+        noise=None,
+        const_noise=False,
+    )
 
-        sample_fn = diffusion.p_sample_loop
+    # Recover XYZ *positions* from HumanML3D vector representation
+    if model.data_rep == 'hml_vec':
+        n_joints = 22 if sample.shape[1] == 263 else 21
+        sample = humanml3d.t2m_dataset.inv_transform(sample.cpu().permute(0, 2, 3, 1)).float()
+        sample = motion_process.recover_from_ric(sample, n_joints)
+        sample = sample.view(-1, *sample.shape[2:]).permute(0, 2, 3, 1)
 
-        sample = sample_fn(
-            model,
-            (batch_size, model.njoints, model.nfeats, max_frames),
-            clip_denoised=False,
-            model_kwargs=model_kwargs,
-            skip_timesteps=0,  # 0 is the default value - i.e. don't skip any step
-            init_image=None,
-            progress=True,
-            dump_steps=None,
-            noise=None,
-            const_noise=False,
-        )
+    kinematic_tree = paramUtil.t2m_kinematic_chain
+    chain_names = ['leg.l', 'leg.r', 'spine', 'arm.l', 'arm.r']
 
-        # Recover XYZ *positions* from HumanML3D vector representation
-        if model.data_rep == 'hml_vec':
-            n_joints = 22 if sample.shape[1] == 263 else 21
-            sample = data_loader.dataset.t2m_dataset.inv_transform(sample.cpu().permute(0, 2, 3, 1)).float()
-            sample = motion_process.recover_from_ric(sample, n_joints)
-            sample = sample.view(-1, *sample.shape[2:]).permute(0, 2, 3, 1)
+    output_motion: np.ndarray = sample.cpu().numpy()[0]
+    output_length = lenbatchTensor.cpu().numpy()[0]
 
-        all_text += model_kwargs['y']['text']
-        all_motions.append(sample.cpu().numpy())
-        all_lengths.append(model_kwargs['y']['lengths'].cpu().numpy())
+    logging.info("created samples")
 
-        logging.info(f"created {len(all_motions) * batch_size} samples")
+    joints = output_motion.transpose(2, 0, 1)[:output_length]
+    data = joints.copy().reshape(len(joints), -1, 3)
+
+    # preparation related to specific datasets
+    if dataset == 'kit':
+        data *= 0.003  # scale for visualization
+    elif dataset == 'humanml':
+        data *= 1.3  # scale for visualization
+    elif dataset in ['humanact12', 'uestc']:
+        data *= -1.5  # reverse axes, scale for visualization
+
+    MINS = data.min(axis=0).min(axis=0)
+    MAXS = data.max(axis=0).max(axis=0)
+
+    frame_count = data.shape[0]
+
+    height_offset = MINS[1]
+    data[:, :, 1] -= height_offset
+    trajec = data[:, 0, [0, 2]]
+
+    data[..., 0] -= data[:, 0:1, 0]
+    data[..., 2] -= data[:, 0:1, 2]
+
+    chain_frames = [
+        {
+            chain_name: list(zip(
+                data[frame, chain, 0],
+                data[frame, chain, 1],
+                data[frame, chain, 2]
+            ))
+            for chain_name, chain in zip(chain_names, kinematic_tree)
+        }
+        for frame in range(frame_count)
+    ]
+
+    with open('chain_frames.json', 'w', encoding='utf-8') as outfile:
+        json.dump(chain_frames, outfile, indent=2, cls=NumpyJSONEncoder)
 
 
 if __name__ == "__main__":
