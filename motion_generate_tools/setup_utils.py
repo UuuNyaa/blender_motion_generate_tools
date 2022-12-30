e598afed8244bf5ca08da92696eb5ef5347959b4
UuuNyaa <uuunyaa@gmail.com>
2022-12-30 06:58:47 -0800

    Ensure pip #1

--- motion_generate_tools/setup_utils.py
+++ motion_generate_tools/setup_utils.py
@@ -80,31 +80,36 @@ def delete_clip_model():
 
 def install_python_modules(use_gpu=False, line_callback: Optional[LineCallback] = None, finally_callback: Optional[FinallyCallback] = None):
     _get_command_executor().exec_command(
-        # force install setuptools
-        sys.executable, '-m', 'pip', 'install',
-        '--disable-pip-version-check',
-        '--no-input',
-        '--ignore-installed',
-        'setuptools',
+        # ensurepip
+        sys.executable, '-m', 'ensurepip',
         line_callback=line_callback,
         finally_callback=lambda e: e.exec_command(
-            # and then install depending modules
+            # force install setuptools
             sys.executable, '-m', 'pip', 'install',
             '--disable-pip-version-check',
             '--no-input',
-            '--upgrade',
-            # '--upgrade-strategy', 'only-if-needed',
-            # '--no-cache-dir',
-            '--exists-action', 'i',
-            # '--ignore-installed',
-            '--extra-index-url', 'https://download.pytorch.org/whl/cu116' if use_gpu else 'https://download.pytorch.org/whl/cpu',
-            'torch',
-            'numpy<1.24.0',
-            'git+https://github.com/openai/CLIP.git',
-            'smplx',
-            'scipy',
-            'chumpy',
-            line_callback=line_callback, finally_callback=finally_callback
+            '--ignore-installed',
+            'setuptools',
+            line_callback=line_callback,
+            finally_callback=lambda e: e.exec_command(
+                # and then install depending modules
+                sys.executable, '-m', 'pip', 'install',
+                '--disable-pip-version-check',
+                '--no-input',
+                '--upgrade',
+                # '--upgrade-strategy', 'only-if-needed',
+                # '--no-cache-dir',
+                '--exists-action', 'i',
+                # '--ignore-installed',
+                '--extra-index-url', 'https://download.pytorch.org/whl/cu116' if use_gpu else 'https://download.pytorch.org/whl/cpu',
+                'torch',
+                'numpy<1.24.0',
+                'git+https://github.com/openai/CLIP.git',
+                'smplx',
+                'scipy',
+                'chumpy',
+                line_callback=line_callback, finally_callback=finally_callback
+            )
         )
     )
 
